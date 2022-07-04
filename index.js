0cdd24a42f127e6e8b281770bacdb6ca7289c4d0
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-07-04 21:46:05 +0300

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import * as navBtn from './modules/spa.js';
-import { DateTime } from './modules/luxon.js';
+// import { DateTime } from './modules/luxon.js';
 
-setInterval(() => {
-  const date = DateTime.now();
-  document.getElementById('date').innerHTML = date.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
-}, 1000);
+// setInterval(() => {
+//   const date = DateTime.now();
+//   document.getElementById('date').innerHTML = date.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+// }, 1000);
 
 const listBtn = document.querySelector('#list');
 const addNewBtn = document.querySelector('#addNew');
