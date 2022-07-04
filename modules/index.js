2a7310e56cbb5f7e7da8585c41d85136a39bcd64
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-07-04 23:16:35 +0300

    Update

--- modules/index.js
+++ modules/index.js
@@ -1,11 +1,12 @@
 import { listBtn as list, addNewBtn as addNew, contactUs as contact } from './spa.js';
-// import { DateTime } from './modules/luxon.js';
+import { DateTime } from './luxon.js';
+import './book.js'
 
-// setInterval(() => {
-//   const date = DateTime.now();
-//   document.getElementById('date').innerHTML =
-// date.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
-// }, 1000);
+setInterval(() => {
+  const date = DateTime.now();
+  document.getElementById('date').innerHTML =
+date.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+}, 1000);
 
 const listBtn = document.querySelector('#list');
 const addNewBtn = document.querySelector('#addNew');
